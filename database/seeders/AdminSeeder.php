21899c4c915c012e050a5da852dbb58fa85e6c79
MariKato <marikato923@gmail.com>
2024-11-30 18:11:42 +0900

    管理者用アカウントを追加

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,9 +14,16 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
+        // 管理者用アカウント
         $admin = new Admin();
         $admin->email = 'admin@example.com';
         $admin->password = Hash::make('nagoyameshi');
+        $admin->save();
+
+        // 課題閲覧用管理者アカウント
+        $asmin = new Admin();
+        $admin->email = 'kadai@example.com';
+        $admin->password = Hash::make('1234pass');
         $admin->save();
     }
 }
